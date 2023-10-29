85c0b3d56053107c8605fad0b6a52fc9397e7006
sayuGitHub <114457066+sayudesu@users.noreply.github.com>
2023-10-30 00:35:12 +0900

    デバッグ用線を描画

--- FIeldDrawer.cpp
+++ FIeldDrawer.cpp
@@ -31,4 +31,33 @@ void FIeldDrawer::Draw()
 {
 	// •`‰æ
 	MV1DrawModel(m_handle);
+
+#if _DEBUG
+	VECTOR start = VGet(1200, 300, 1200);
+	VECTOR end = VGet(-1200, 300, 1200);
+	DrawLine3D(start, end, 0xffffff);
+
+	start = VGet(-1200, 300, -1200);
+	end = VGet(1200, 300, -1200);
+	DrawLine3D(start, end, 0xffffff);
+
+	start = VGet(1200, 300, -1200);
+	end = VGet(1200, 300, 1200);
+	DrawLine3D(start, end, 0xffffff);
+
+	start = VGet(-1200, 300, 1200);
+	end = VGet(-1200, 300, -1200);
+	DrawLine3D(start, end, 0xffffff);
+	// Ž²
+	VECTOR Xp = ConvWorldPosToScreenPos(VGet(1000, 300, 0));
+	VECTOR Xm = ConvWorldPosToScreenPos(VGet(-1000, 300, 0));
+
+	VECTOR Zp = ConvWorldPosToScreenPos(VGet(0, 300, 1000));
+	VECTOR Zm = ConvWorldPosToScreenPos(VGet(0, 300, -1000));
+
+	DrawString(Xp.x, Xp.y, "X+", 0xffffff);
+	DrawString(Xm.x, Xm.y, "X-", 0xffffff);
+	DrawString(Zp.x, Zp.y, "Z+", 0xffffff);
+	DrawString(Zm.x, Zm.y, "Z-", 0xffffff);
+#endif
 }
