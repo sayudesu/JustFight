ba506a7921b67eb7ae082474cfaa8f79440c1d8c
sayuGitHub <114457066+sayudesu@users.noreply.github.com>
2024-06-20 10:14:23 +0900

    ブラークラスのコメント追加とコードの修正

--- Util/BlurScreen.cpp
+++ Util/BlurScreen.cpp
@@ -8,11 +8,9 @@ EffectScreen* EffectScreen::m_pInstance = nullptr;
 
 void EffectScreen::Load()
 {
-#if true		
-
 	// 攻撃演出用
-	m_quakeX = 0.0f;
-	m_quakeFrame = 0;
+	m_shake = 0.0f;
+	m_shakeFrame = 0;
 
 	// 加工用の画面ハンドルを保存
 	int sw = 0, sh = 0, bit = 0;      // 画面幅＆画面高＆ビット数
@@ -22,12 +20,12 @@ void EffectScreen::Load()
 	{
 		m_damageScreen[i] = MakeScreen(sw, sh);// 加工用画面を用意する
 	}
-#endif
 	// モーションブラー用
 	m_notBlendDraw = 0;
 	m_current = 0;
 	m_alpha = 150;
 
+	// 画像ハンドルを作成する
 	for (int i = 0; i < static_cast<int>(ScreenEffectNo::MAX); ++i)
 	{
 		m_blurScreen[i] = MakeScreen(Game::kScreenWidth, Game::kScreenHeight);
@@ -36,7 +34,7 @@ void EffectScreen::Load()
 
 void EffectScreen::Unload()
 {
-#if true	
+	// 画像ハンドルを削除する
 	for (int i = 0; i < 3; ++i)
 	{
 		if (m_damageScreen[i] != -1)
@@ -44,27 +42,23 @@ void EffectScreen::Unload()
 			DeleteGraph(m_damageScreen[i]);
 		}
 	}
-#endif
 }
 
 void EffectScreen::ClearScreen()
 {
-#if true	
 	// 画面をクリアにする
 	ClearDrawScreen();
-#endif
 }
 
 void EffectScreen::ScreenBack()
 {
-#if true
 	SetDrawScreen(DX_SCREEN_BACK);
-#endif
+
 }
 
 void EffectScreen::BlurIReplayInit()
 {
-#if true	
+	// 画像ハンドルを初期化し、新しい画像ハンドルを作成する	
 	for (int i = 0; i < static_cast<int>(ScreenEffectNo::MAX); ++i)
 	{
 		if (m_blurScreen[i] != -1)
@@ -76,11 +70,11 @@ void EffectScreen::BlurIReplayInit()
 	}
 	m_current = 0;
 	m_notBlendDraw = 0;
-#endif
 }
 
 void EffectScreen::BlurRelease()
 {
+	// 画像ハンドルを削除する
 	for (int i = 0; i < static_cast<int>(ScreenEffectNo::MAX); ++i)
 	{
 		if (m_blurScreen[i] != -1)
@@ -92,15 +86,12 @@ void EffectScreen::BlurRelease()
 
 void EffectScreen::BlurPreRenderBlurScreen()
 {
-#if true	
 	// 画面の作成
 	SetDrawScreen(m_blurScreen[m_current]);
-#endif
 }
 
 void EffectScreen::BlurPostRenderBlurScreen(bool isBlurDraw)
 {
-#if true	
 	// アルファ値変更用
 	int alphaRate = 0;
 	// 描画する画面のフレーム
@@ -139,96 +130,50 @@ void EffectScreen::BlurPostRenderBlurScreen(bool isBlurDraw)
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	// 画面揺れ効果があるかどうか
-	if (static_cast<int>(m_quakeX) != 0)
+	if (static_cast<int>(m_shake) != 0)
 	{
 		SetDrawBlendMode(DX_BLENDMODE_ADD, 128);
 	
 		// 加算合成する
-		DrawGraph(m_quakeX, m_quakeX, m_blurScreen[m_current], false);
+		DrawGraph(m_shake, m_shake, m_blurScreen[m_current], false);
 
 		// ガウスでぼかしを入れる
 		GraphFilter(m_damageScreen[static_cast<int>(DamageEffectNo::QUAKE)], DX_GRAPH_FILTER_GAUSS, 32, 1400);
 	}
 
 	// ブラー効果を得た画像を描画する
-	DrawGraph(m_quakeX, m_quakeX, m_blurScreen[m_current], false);
+	DrawGraph(m_shake, m_shake, m_blurScreen[m_current], false);
 
 	// ブラー効果用に使用していなかった配列の番号を計算する
 	m_current = 1 - m_current;
 
 	// 画面揺れ効果があるかどうか
-	if (static_cast<int>(m_quakeX) != 0)
+	if (static_cast<int>(m_shake) != 0)
 	{
 		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 128);// ブレンドモードを初期状態に戻す
 	}
-#endif
 }
 
 void EffectScreen::QuakeReplayInit()
-{
-#if true	
-	m_quakeX     = 10.0f;
-	m_quakeFrame = 30;
-#endif
+{	
+	m_shake      = 10.0f;
+	m_shakeFrame = 30;
 }
 
 void EffectScreen::QuakeUpdate()
-{
-#if true	
-	if (m_quakeFrame > 0) 
+{	
+	// 画面の揺れ具合の調整
+	if (m_shakeFrame > 0) 
 	{
-		m_quakeX = -m_quakeX;
-		m_quakeX *= 0.95f;
-		m_quakeFrame--;
+		m_shake = -m_shake;
+		m_shake *= 0.95f;
+		m_shakeFrame--;
 	}
 	else
 	{
-		m_quakeX = 0.0f;
+		m_shake = 0.0f;
 	}
 
 	m_blendAddRate = 128 / 30;
-	m_blendAddRate *= m_quakeFrame;
-#endif
-}
-
-void EffectScreen::QuakeRelease()
-{
-#if true
-	DeleteGraph(m_damageScreen[static_cast<int>(DamageEffectNo::QUAKE)]);
-#endif
-}
-
-void EffectScreen::QuakePreRenderBlurScreen()
-{
-#if true
-	// 画面の保存を行う
-	SetDrawScreen(m_damageScreen[static_cast<int>(DamageEffectNo::QUAKE)]);
-#endif
-}
-
-void EffectScreen::QuakePostRenderBlurScreen()
-{
-#if true
-
-	/*SetDrawScreen(m_damageScreen[static_cast<int>(DamageEffectNo::QUAKE)]);
-	ClearDrawScreen();
-	SetDrawScreen(DX_SCREEN_BACK);*/
-
-	if (m_quakeFrame > 0)
-	{
-
-//		GraphFilter(m_screen[static_cast<int>(EffectNo::QUAKE)], DX_GRAPH_FILTER_INVERT);         // 色を反転させる
-//		GraphFilter(m_screen[static_cast<int>(EffectNo::QUAKE)], DX_GRAPH_FILTER_MONO, 128, 0);   // 各ピクセルの色をＲＧＢ形式からYCbCr形式に変換し引数の Cb Cr の値を置き換えた後、再びＲＧＢ形式に戻す
-//      GraphFilter(m_screen[static_cast<int>(EffectNo::QUAKE)], DX_GRAPH_FILTER_GAUSS, 16, 1400);// ガウスでぼかしを入れる
-		SetDrawBlendMode(DX_BLENDMODE_ADD, 128);                                                  // 加算合成する
-		DrawGraph(m_quakeX, m_quakeX, m_damageScreen[static_cast<int>(DamageEffectNo::QUAKE)], false);        // 画面を描画
-		GraphFilter(m_damageScreen[static_cast<int>(DamageEffectNo::QUAKE)], DX_GRAPH_FILTER_GAUSS, 32, 1400);// ガウスでぼかしを入れる
-		DrawGraph(m_quakeX, m_quakeX, m_damageScreen[static_cast<int>(DamageEffectNo::QUAKE)], false);        // 画面を描画
-		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 128);                                              // ブレンドモードを初期状態に戻す
-	}
-	else
-	{
-		DrawGraph(0, 0, m_damageScreen[static_cast<int>(DamageEffectNo::QUAKE)], false);                      // 通常画面描画
-	}
-#endif
+	m_blendAddRate *= m_shakeFrame;
 }
--- Util/BlurScreen.h
+++ Util/BlurScreen.h
@@ -8,10 +8,10 @@ private:
 public:
 	virtual~EffectScreen() = default;
 	// コピーコンストラクタの禁止
-	EffectScreen(const EffectScreen&) = delete;
+	EffectScreen(const EffectScreen&)            = delete;
 	EffectScreen& operator=(const EffectScreen&) = delete;
-	EffectScreen(EffectScreen&&) = delete;
-	EffectScreen& operator=(EffectScreen&&) = delete;
+	EffectScreen(EffectScreen&&)                 = delete;
+	EffectScreen& operator=(EffectScreen&&)      = delete;
 
 	// インスタンスの作成
 	static EffectScreen& GetInstance()
@@ -48,9 +48,6 @@ public:
 	// 揺らし
 	void QuakeReplayInit();
 	void QuakeUpdate();
-	void QuakeRelease();
-	void QuakePreRenderBlurScreen();
-	void QuakePostRenderBlurScreen();
 
 private:
 	enum class DamageEffectNo
@@ -63,9 +60,10 @@ private:
 	int m_damageScreen[static_cast<int>(DamageEffectNo::MAX)];
 
 	// 画面揺れ用
-	float m_quakeX;
-	int m_quakeFrame;
+	float m_shake;
+	int m_shakeFrame;
 
+	// ブラーを変える場合の重ねる画面の数
 	enum class ScreenEffectNo
 	{
 		ONE,
@@ -78,11 +76,9 @@ private:
 	int m_blurScreen[static_cast<int>(ScreenEffectNo::MAX)];
 
 	// ブラー専用
-	int m_current;
-	int m_alpha;
-	int m_notBlendDraw;
-
-
+	int m_current = 0;
+	int m_alpha   = 0;
+	int m_notBlendDraw = 0;
 	int m_blendAddRate = 0;
 };
 
